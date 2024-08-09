e1bf0d66a0933779d5405184bf8ca6b09bd5e140
Andrew Fuller <fuller.andrew55@gmail.com>
2024-08-09 16:16:29 -0500

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name: "Andrew",
+    streetAddress: "lalala",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+  
+    newObj[key] = value;
+  
+    return newObj;
+  }
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Sam")
+
+function beforeEach() {
+    for (const key in employee) {
+      delete employee[key];
+    }
+  
+    employee.name = 'Sam';
+  };
+  
+  beforeEach()
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+
+  const newEmployee2 = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')
+
+
+  function deleteFromEmployeeByKey(obj){
+    const newObj = { ...obj };
+  
+    delete newObj.name
+  
+    return newObj
+  }
+
+
+  let newEmployee3 = deleteFromEmployeeByKey(employee, "name")
+
+  beforeEach()
+
+  function destructivelyDeleteFromEmployeeByKey(obj){
+    delete obj.name
+
+    return obj
+  }
+
+  let newEmployee4 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name")
